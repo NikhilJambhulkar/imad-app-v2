e73fb59412a32cabb237aaa8e9e06a7b0f4661a7
NikhilJambhulkar <nikhiljambhulkars@gmail.com>
2017-02-06 11:56:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,11 +22,25 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-   var names=['name1','name1','name1'] ;
-   var list='';
-   for(var i=0;i<names.length;i++){
-       list+='<li>'+names[i]+'</li>'
-   }
-   var ul=document.getElementById('namelist');
-   ul.innerHTML=list;
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readystatechange===XMLHttpRequest.DONE){
+            if(request.status===200){
+                   var names=['name1','name1','name1'] ;
+                   var list='';
+                   for(var i=0;i<names.length;i++){
+                       list+='<li>'+names[i]+'</li>'
+                    }
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
+            }
+        }
+    };
+    
+    request.open('GET','http://nikhiljambhulkar.imad.hasura-app.io/submit-name='+name,true);
+    request.send(null);
+    
+
 };
