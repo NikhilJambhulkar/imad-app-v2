c0b11d12412d31299dd0d1671696bd1d3e306af3
NikhilJambhulkar <nikhiljambhulkars@gmail.com>
2017-02-11 22:12:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,7 +58,7 @@ commenttext.onclick=function(){
                    comments=JSON.parse(comments);
                    var list='';
                    for(var i=0;i<comments.length;i++){
-                       list+='<p>'+comments[i]+'</p><hr/>'
+                       list+='<p><b>*</b>'+comments[i]+'</p><hr/>'
                     }
         var div=document.getElementById('commentdiv');
         div.innerHTML=list;
