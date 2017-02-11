80f349a8d99143d5a30c7c6bca47f73de13822ae
NikhilJambhulkar <nikhiljambhulkars@gmail.com>
2017-02-11 21:07:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ var comments=[];
 app.get('/submit-comment', function (req, res) {
     var comment=req.query.comment;
     comments.push(name);
-    res.send(JSON.stringify(names));
+    res.send(JSON.stringify(comments));
 });
 
 var names=[];
